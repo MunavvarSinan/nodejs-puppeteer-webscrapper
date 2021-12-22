d5ff3d933b4462ad6f6832525b4e905eae23c8bb
MunavvarSinan <munavvarsinan8@gmail.com>
2021-12-22 22:46:46 +0530

    minor changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
